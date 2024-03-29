4ae90a5546c7c3f4fc79d2eee5f3a0a1303915b2
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-03-29 10:07:19 -0400

    Configure python-chrisapp-template/bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,9 +15,9 @@
 # ----------------------------------------
 
 PLUGIN_NAME="$(basename $(dirname $(realpath $0)))"  # name of current directory
-PLUGIN_TITLE='My ChRIS Plugin'
-SCRIPT_NAME='commandname'
-DESCRIPTION='A ChRIS plugin to do something awesome'
+PLUGIN_TITLE='Spleen 3D image segmentation training (MONAI)'
+SCRIPT_NAME='spleenseg_train'
+DESCRIPTION="A ChRIS based off project MONAI's spleen segmenation notebook (training only)"
 ORGANIZATION='FNNDSC'
 EMAIL='dev@babyMRI.org'
 
@@ -46,7 +46,7 @@ ENABLE_ACTIONS_BUILD=yes
 # STEP 2. Uncomment the line where it says READY=yes
 # ----------------------------------------
 
-#READY=yes
+READY=yes
 
 # ----------------------------------------
 # STEP 3. Run: ./bootstrap.sh
