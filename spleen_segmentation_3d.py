388950691b424ba3d1912d3032868e83595dadfa
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-04-04 16:08:19 -0400

    Assume data already downloaded

--- spleen_segmentation_3d.py
+++ spleen_segmentation_3d.py
@@ -44,17 +44,17 @@
 # In[1]:
 
 
-get_ipython().system(
-    'python -c "import monai" || pip install -q "monai-weekly[gdown, nibabel, tqdm, ignite]"'
-)
-get_ipython().system('python -c "import matplotlib" || pip install -q matplotlib')
-get_ipython().run_line_magic("matplotlib", "inline")
+# get_ipython().system(
+#     'python -c "import monai" || pip install -q "monai-weekly[gdown, nibabel, tqdm, ignite]"'
+# )
+# get_ipython().system('python -c "import matplotlib" || pip install -q matplotlib')
+# get_ipython().run_line_magic("matplotlib", "inline")
 
 
 # ## Setup imports
 
 # In[2]:
-
+import pudb
 
 import os
 from monai.utils import first, set_determinism
@@ -113,13 +113,13 @@ print(root_dir)
 # In[5]:
 
 
-resource = "https://msd-for-monai.s3-us-west-2.amazonaws.com/Task09_Spleen.tar"
-md5 = "410d4a301da4e5b2f6f86ec3ddba524e"
-
-compressed_file = os.path.join(root_dir, "Task09_Spleen.tar")
+# resource = "https://msd-for-monai.s3-us-west-2.amazonaws.com/Task09_Spleen.tar"
+# md5 = "410d4a301da4e5b2f6f86ec3ddba524e"
+#
+# compressed_file = os.path.join(root_dir, "Task09_Spleen.tar")
 data_dir = os.path.join(root_dir, "Task09_Spleen")
-if not os.path.exists(data_dir):
-    download_and_extract(resource, compressed_file, root_dir, md5)
+# if not os.path.exists(data_dir):
+#     download_and_extract(resource, compressed_file, root_dir, md5)
 
 
 # ## Set MSD Spleen dataset path
@@ -220,7 +220,7 @@ val_transforms = Compose(
     ]
 )
 
-
+pudb.set_trace()
 # ## Check transforms in DataLoader
 
 # In[9]:
