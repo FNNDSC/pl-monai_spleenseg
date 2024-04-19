8cfc6c151f3fecdaf28825fcdee1d08723d2343d
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-04-19 10:54:07 -0400

    Add post infer callbacks and use data model module

--- spleenseg/core/neuralnet.py
+++ spleenseg/core/neuralnet.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
-from collections.abc import Iterable
-from pathlib import Path
+# from collections.abc import Iterable
+# from pathlib import Path
 from argparse import Namespace
-from dataclasses import dataclass, field
+from collections.abc import Callable
 
-import os, sys
+# from dataclasses import dataclass, field
+
+# import os, sys
+# from monai.transforms import transform
 from monai.transforms.compose import Compose
-from monai.utils import first, set_determinism
+
+# from monai.utils import first, set_determinism
 
 # from monai.transforms import (
 #     AsDiscrete,
@@ -24,20 +28,23 @@ from monai.utils import first, set_determinism
 #     Spacingd,
 #     Invertd,
 # )
-from monai.handlers.utils import from_engine
-from monai.networks.nets.unet import UNet
+# from monai.handlers.utils import from_engine
+# from monai.networks.nets.unet import UNet
 
 # from monai.transforms import LoadImage
-from monai.networks.layers.factories import Norm
-from monai.metrics.meandice import DiceMetric
-from monai.losses.dice import DiceLoss
+# from monai.networks.layers.factories import Norm
+# from monai.metrics.meandice import DiceMetric
+# from monai.losses.dice import DiceLoss
 from monai.inferers.utils import sliding_window_inference
 from monai.data.dataset import CacheDataset
 from monai.data.dataloader import DataLoader
-from monai.data.dataset import Dataset
+
+# from monai.data.dataset import Dataset
 from monai.data.utils import decollate_batch
-from monai.config.deviceconfig import print_config
-from monai.apps.utils import download_and_extract
+from monai.data.meta_tensor import MetaTensor
+
+# from monai.config.deviceconfig import print_config
+# from monai.apps.utils import download_and_extract
 import torch
 
 # import matplotlib.pyplot as plt
@@ -45,72 +52,80 @@ import torch
 # import shutil
 # import glob
 # import pudb
-from typing import Any, Optional, Callable
+from typing import Any
 import numpy as np
 
-
-@dataclass
-class LoaderCache:
-    loader: DataLoader
-    cache: CacheDataset
+from spleenseg.transforms import transforms
+from spleenseg.models import data
+from spleenseg.plotting import plotting
 
 
-@dataclass
-class TrainingParams:
-    max_epochs: int = 600
-    val_interval = 2
-    best_metric: float = -1.0
-    best_metric_epoch = -1
-    modelPth: Path = Path("")
-    modelONNX: Path = Path("")
-    determinismSeed: int = 0
-
-    def __init__(self, options: Namespace):
-        self.options = options
-        if options is not None:
-            self.max_epochs = self.options.maxEpochs
-            self.modelPth = Path(options.outputdir) / "model.pth"
-            self.modelONNX = Path(options.outputdir) / "model.onnx"
-            self.determinismSeed = self.options.determinismSeed
-            set_determinism(seed=self.determinismSeed)
-
-
-@dataclass
-class TrainingLog:
-    loss_per_epoch: list[float] = field(default_factory=list)
-    metric_per_epoch: list[float] = field(default_factory=list)
-
-
-@dataclass
-class ModelParams:
-    optimizer: torch.optim.Adam
-    device: torch.device = torch.device("cpu")
-    model: UNet = UNet(
-        spatial_dims=3,
-        in_channels=1,
-        out_channels=2,
-        channels=(16, 32, 64, 128, 256),
-        strides=(2, 2, 2, 2),
-        num_res_units=2,
-        norm=Norm.BATCH,
-    )
-    fn_loss: Callable[[torch.Tensor, torch.Tensor], torch.Tensor] = DiceLoss(
-        to_onehot_y=True, softmax=True
-    )
-    dice_metric: DiceMetric = DiceMetric(include_background=False, reduction="mean")
-
-    def __init__(self, options: Namespace):
-        self.options = options
-        if options is not None:
-            self.device = torch.device(self.options.device)
-            torch.manual_seed(42)
-            self.model = self.model.to(self.device)
-            self.optimizer = torch.optim.Adam(self.model.parameters(), 1e-4)
+# @dataclass
+# class LoaderCache:
+#     loader: DataLoader
+#     cache: CacheDataset
+#
+#
+# @dataclass
+# class TrainingParams:
+#     max_epochs: int = 600
+#     val_interval = 2
+#     best_metric: float = -1.0
+#     best_metric_epoch = -1
+#     modelPth: Path = Path("")
+#     modelONNX: Path = Path("")
+#     determinismSeed: int = 0
+#
+#     def __init__(self, options: Namespace):
+#         self.options = options
+#         if options is not None:
+#             self.max_epochs = self.options.maxEpochs
+#             self.modelPth = Path(options.outputdir) / "model.pth"
+#             self.modelONNX = Path(options.outputdir) / "model.onnx"
+#             self.determinismSeed = self.options.determinismSeed
+#             set_determinism(seed=self.determinismSeed)
+#
+#
+# @dataclass
+# class TrainingLog:
+#     loss_per_epoch: list[float] = field(default_factory=list)
+#     metric_per_epoch: list[float] = field(default_factory=list)
+#
+#
+# @dataclass
+# class ModelParams:
+#     optimizer: torch.optim.Adam
+#     device: torch.device = torch.device("cpu")
+#     model: UNet = UNet(
+#         spatial_dims=3,
+#         in_channels=1,
+#         out_channels=2,
+#         channels=(16, 32, 64, 128, 256),
+#         strides=(2, 2, 2, 2),
+#         num_res_units=2,
+#         norm=Norm.BATCH,
+#     )
+#     fn_loss: Callable[[torch.Tensor, torch.Tensor], torch.Tensor] = DiceLoss(
+#         to_onehot_y=True, softmax=True
+#     )
+#     dice_metric: DiceMetric = DiceMetric(include_background=False, reduction="mean")
+#
+# def __init__(self, options: Namespace):
+#     self.options = options
+#     if options is not None:
+#         self.device = torch.device(self.options.device)
+#         torch.manual_seed(42)
+#         self.model = self.model.to(self.device)
+#         self.optimizer = torch.optim.Adam(self.model.parameters(), 1e-4)
 
 
 def tensor_desc(
     T: torch.Tensor | tuple[torch.Tensor, ...] | dict[Any, torch.Tensor], **kwargs
 ) -> torch.Tensor:
+    """
+    A simple method to "characterize" or describe a (possibly high dimensional) tensor
+    as some 2D structure.
+    """
     strAs: str = "meanstd"
     v1: float = 0.0
     v2: float = 0.0
@@ -133,25 +148,26 @@ def tensor_desc(
 
 class NeuralNet:
     def __init__(self, options: Namespace):
-        self.network: ModelParams = ModelParams(options)
-        self.training: TrainingParams = TrainingParams(options)
+        self.network: data.ModelParams = data.ModelParams(options)
+        self.trainingParams: data.TrainingParams = data.TrainingParams(options)
 
         self.input: torch.Tensor | tuple[torch.Tensor, ...] | dict[Any, torch.Tensor]
         self.output: torch.Tensor | tuple[torch.Tensor, ...] | dict[Any, torch.Tensor]
         self.target: torch.Tensor | tuple[torch.Tensor, ...] | dict[Any, torch.Tensor]
 
-        self.trainingLog: TrainingLog = TrainingLog()
+        self.trainingLog: data.TrainingLog = data.TrainingLog()
+        self.trainingEpoch: int = 0
 
         self.f_outputPost: Compose
         self.f_labelPost: Compose
 
-        self.trainingSpace: LoaderCache
-        self.validationSpace: LoaderCache
-        self.novelSpace: LoaderCache
+        self.trainingSpace: data.LoaderCache
+        self.validationSpace: data.LoaderCache
+        self.novelSpace: data.LoaderCache
 
     def loaderCache_create(
         self, fileList: list[dict[str, str]], transforms: Compose, batch_size: int = 2
-    ) -> LoaderCache:
+    ) -> data.LoaderCache:
         """
         ## Define CacheDataset and DataLoader for training and validation
 
@@ -180,7 +196,7 @@ class NeuralNet:
         else:
             loader = DataLoader(ds, batch_size=batch_size, num_workers=4)
 
-        loaderCache: LoaderCache = LoaderCache(cache=ds, loader=loader)
+        loaderCache: data.LoaderCache = data.LoaderCache(cache=ds, loader=loader)
         return loaderCache
 
     def tensor_assign(
@@ -223,7 +239,7 @@ class NeuralNet:
         self.network.optimizer.step()
         return f_loss.item()
 
-    def train_overSampleSpace_retLoss(self, trainingSpace: LoaderCache) -> float:
+    def train_overSampleSpace_retLoss(self, trainingSpace: data.LoaderCache) -> float:
         sample: int = 0
         sample_loss: float = 0.0
         total_loss: float = 0.0
@@ -240,7 +256,8 @@ class NeuralNet:
                 and trainingSpace.loader.batch_size is not None
             ):
                 print(
-                    f"{sample:02}/{len(trainingSpace.cache) // trainingSpace.loader.batch_size}, "
+                    f"    training run {sample:02}/"
+                    f"{len(trainingSpace.cache) // trainingSpace.loader.batch_size}, "
                     f"sample loss: {sample_loss:.4f}"
                 )
         total_loss /= sample
@@ -248,48 +265,121 @@ class NeuralNet:
 
     def train(
         self,
-        trainingSpace: LoaderCache | None = None,
-        validationSpace: LoaderCache | None = None,
+        trainingSpace: data.LoaderCache | None = None,
+        validationSpace: data.LoaderCache | None = None,
     ):
-        epoch: int = 0
+        self.f_outputPost = transforms.transforms_build(
+            [transforms.f_AsDiscreteArgMax()]
+        )
+        self.f_labelPost = transforms.transforms_build([transforms.f_AsDiscrete()])
+        self.trainingEpoch: int = 0
         epoch_loss: float = 0.0
         if trainingSpace:
             self.trainingSpace = trainingSpace
         if validationSpace:
             self.validationSpace = validationSpace
-        for epoch in range(self.training.max_epochs):
+        for self.trainingEpoch in range(self.trainingParams.max_epochs):
             print("-" * 10)
-            print(f"epoch {epoch+1:03} / {self.training.max_epochs}")
+            print(f"epoch {self.trainingEpoch+1:03} / {self.trainingParams.max_epochs}")
             self.network.model.train()
             epoch_loss = self.train_overSampleSpace_retLoss(self.trainingSpace)
-            print(f"epoch {epoch+1:03}, average loss: {epoch_loss:.4f}")
+            print(f"epoch {self.trainingEpoch+1:03}, average loss: {epoch_loss:.4f}")
             self.trainingLog.loss_per_epoch.append(epoch_loss)
-            if (epoch + 1) % self.training.val_interval == 0:
-                self.slidingWindowInference_do(self.validationSpace, epoch)
+            if (self.trainingEpoch + 1) % self.trainingParams.val_interval == 0:
+                print("evaluating current model")
+                self.slidingWindowInference_do(self.validationSpace, self.validate)
+        print("-" * 10)
+        print(
+            "Training complete: "
+            "best metric: {self.trainingLog.best_metric:.4f} "
+            "at epoch: {self.trainingLog.best_metric_epoch}"
+        )
 
-    def inference_metricsProcess(self, epoch: int) -> float:
+    def inference_metricsProcess(self) -> float:
         metric: float = self.network.dice_metric.aggregate().item()
         self.trainingLog.metric_per_epoch.append(metric)
         self.network.dice_metric.reset()
-        if metric > self.training.best_metric:
-            self.training.best_metric = metric
-            self.training.best_metric_epoch = epoch + 1
-            torch.save(self.network.model.state_dict(), str(self.training.modelPth))
-            print("saved new best metric model")
+        if metric > self.trainingLog.best_metric:
+            self.trainingLog.best_metric = metric
+            self.trainingLog.best_metric_epoch = self.trainingEpoch + 1
+            torch.save(
+                self.network.model.state_dict(), str(self.trainingParams.modelPth)
+            )
+            print("  (saved new best metric model)")
         print(
-            f"current epoch: {epoch + 1}, current mean dice: {metric:.4f}"
-            f"\nbest mean dice: {self.training.best_metric:.4f}"
-            f"at epoch: {self.training.best_metric_epoch}"
+            f"    current mean dice: {metric:.4f}"
+            f"\n       best mean dice: {self.trainingLog.best_metric:.4f} "
+            f"\n           best epoch: {self.trainingLog.best_metric_epoch:03} "
         )
         return metric
 
+    def diceMetric_do(
+        self, outputPostProc: list[MetaTensor], truth: torch.Tensor
+    ) -> torch.Tensor:
+        labelPostProc = [
+            self.f_labelPost(i)
+            for i in decollate_batch(truth)  # type: ignore[arg-type]
+        ]
+        Tdm: torch.Tensor = self.network.dice_metric(
+            y_pred=outputPostProc,  # type: ignore
+            y=labelPostProc,  # type: ignore
+        )
+        return Tdm
+
+    def validate(
+        self,
+        sample: dict[str, MetaTensor | torch.Tensor],
+        space: data.LoaderCache,
+        index: int,
+        result: torch.Tensor,
+    ) -> float:
+        """
+        This is callback method called in the inference stage.
+
+
+        Given a 'sample' from a LoaderCache iteration, a data space containing
+        the sample, the sample 'index', and the result, perform some validation.
+        """
+        metric: float = -1.0
+        outputPostProc: list[MetaTensor] = [
+            self.f_outputPost(i)
+            for i in decollate_batch(result)  # type: ignore[arg-type]
+        ]
+        Tdm: torch.Tensor = self.diceMetric_do(
+            outputPostProc, sample["label"].to(self.network.device)
+        )
+        if space.loader.batch_size:
+            print(
+                f"  validation run {index:02}/"
+                f"{len(space.cache) // space.loader.batch_size:02}, "
+                f"dice metric: {Tdm}"
+            )
+            if index == len(space.cache) // space.loader.batch_size:
+                metric = self.inference_metricsProcess()
+        return metric
+
     def slidingWindowInference_do(
-        self, inferCache: LoaderCache, epoch: int | None = None
+        self,
+        inferSpace: data.LoaderCache,
+        f_callback: (
+            Callable[
+                [
+                    dict[str, MetaTensor | torch.Tensor],
+                    data.LoaderCache,
+                    int,
+                    torch.Tensor,
+                ],
+                float,
+            ]
+            | None
+        ) = None,
     ) -> float:
         metric: float = 0.0
         self.network.model.eval()
+        index: int = 0
         with torch.no_grad():
-            for sample in inferCache.loader:
+            for sample in inferSpace.loader:
+                index += 1
                 input: torch.Tensor = sample["image"].to(self.network.device)
                 roi_size: tuple[int, int, int] = (160, 160, 160)
                 sw_batch_size: int = 4
@@ -298,20 +388,52 @@ class NeuralNet:
                         input, roi_size, sw_batch_size, self.network.model
                     )
                 )
-                outputPostProc = [
-                    self.f_outputPost(i)
-                    for i in decollate_batch(outputRaw)  # type: ignore[arg-type]
-                ]
-                if epoch is not None:
-                    labelTruth: torch.Tensor = sample["label"].to(self.network.device)
-                    labelPostProc = [
-                        self.f_labelPost(i)
-                        for i in decollate_batch(labelTruth)  # type: ignore[arg-type]
-                    ]
-                    self.network.dice_metric(
-                        y_pred=outputPostProc,  # type: ignore
-                        y=labelPostProc,  # type: ignore
-                    )
-            if epoch is not None:
-                metric = self.inference_metricsProcess(epoch)
+                if f_callback is not None:
+                    metric = f_callback(sample, inferSpace, index, outputRaw)
         return metric
+
+    def plot_bestModel(
+        self,
+        sample: dict[str, MetaTensor | torch.Tensor],
+        space: data.LoaderCache,
+        index: int,
+        result: torch.Tensor,
+    ) -> float:
+        print(f"Saving best model applied to validation sample {index}")
+        plotting.plot_bestModelOnValidate(
+            sample,
+            result,
+            str(index),
+            self.trainingParams.outputDir / f"bestModel-val-{index}.png",
+        )
+        return 0.0
+
+    def diceMetric_onValidationSpacing(
+        self,
+        sample: dict[str, MetaTensor | torch.Tensor],
+        space: data.LoaderCache,
+        index: int,
+        result: torch.Tensor,
+    ) -> float:
+        sample["pred"] = result
+        sample = [
+            self.f_outputPost(i)
+            for i in decollate_batch(sample)  # type: ignore[arg-type]
+        ]
+        self.network.model.load_state_dict(
+            torch.load(str(self.trainingParams.modelPth))
+        )
+        self.slidingWindowInference_do(self.validationSpace, self.plot_bestModel)
+        return 0.0
+
+    def bestModel_evaluateImageSpacings(self, validationTransforms: Compose):
+        self.network.model.load_state_dict(
+            torch.load(str(self.trainingParams.modelPth))
+        )
+        self.f_outputPost = transforms.transforms_build(
+            [
+                transforms.f_Invertd(validationTransforms),
+                transforms.f_predAsDiscreted(),
+                transforms.f_labelAsDiscreted(),
+            ]
+        )
