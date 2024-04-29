5fea5b8fcaaa0ce18f113a02a2527bc474e34c49
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-04-29 11:15:07 -0400

    Bump version to 1.2.16

--- spleenseg/spleenseg.py
+++ spleenseg/spleenseg.py
@@ -41,7 +41,7 @@ DISPLAY_TITLE = r"""
 ╚══════╝╚═╝     ╚══════╝╚══════╝╚══════╝╚═╝  ╚═══╝╚══════╝╚══════╝ ╚═════╝
 """
 
-__version__ = "1.2.14"
+__version__ = "1.2.16"
 import spleenseg.splparser as spl
 
 description: str = """
